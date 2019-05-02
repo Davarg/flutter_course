5fb0b54e08c3f2eb8c6982b775fb244fbf1cdb5c
Alex Makushkin <maka-dava@yandex.ru>
2019-05-02 22:29:57 +0300

    features(Routes): created boilerplate code for named routes. Code not working

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-import './pages/auth.dart';
+// import './pages/auth.dart';
+import './pages/products_admin.dart';
+import './pages/products.dart';
 
 /*void main() {
   runApp(MyApp());
@@ -15,7 +17,29 @@ class MyApp extends StatelessWidget {
           brightness: Brightness.light,
           primarySwatch: Colors.deepOrange,
           accentColor: Colors.deepPurple),
-      home: AuthPage(),
+      // home: AuthPage(),
+      routes: {
+        '/': (BuildContext context) => ProductsPage(),
+        '/admin': (BuildContext context) => ProductsAdminPage(),
+      },
+      onGenerateRoute: (RouteSettings settings) {
+        final List<String> pathElements = settings.name.split('/');
+
+        if (pathElements[0] != '') {
+          return null;
+        }
+
+        if (pathElements[1] == 'product') {
+          final int index = int.parse(pathElements[2]);
+          
+          return MaterialPageRoute(
+            builder: (BuildContext context) =>
+                ProductPage(products[index]['title'], products[index]['image']),
+          );
+        }
+
+        return null;
+      },
     );
   }
 }
